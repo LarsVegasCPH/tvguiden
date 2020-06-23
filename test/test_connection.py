ca87bc974f699e9bd5e52d5ce4ca35a032b21318
LarsVegasCPH <bjoern@goettler.dk>
2020-06-23 16:40:45 +0200

    Test passes

--- test/test_connection.py
+++ test/test_connection.py
@@ -1,5 +1,7 @@
-import tvguiden
+"""For some reason I don't understand, this only works if I do "python3 -m pytest" """
+from tvguiden import tvguiden
 import requests
+
 
 def test_baseurl():
     response = requests.head(url=tvguiden._baseURL)
